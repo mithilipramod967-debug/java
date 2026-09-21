0de0267769ced667ce9f55a362570bee0d93aa9d
Mithili Pramod <mithilipramod967@gmail.com>
2026-09-21 17:27:47 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,31 +1,17 @@
-import java.util.Scanner;
+import student.Student;
+import faculty.Faculty;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-             System.out.print("Enter Employee ID: ");
-             Integer id = Integer.valueOf(sc.nextLine());
+        Student s = new Student(101, "Rahul", "Computer Engineering");
 
-             System.out.print("Enter Basic Salary: ");
-             Double salary = Double.valueOf(sc.nextLine());
+        Faculty f = new Faculty(501, "Dr. Sharma", "Computer Engineering");
 
-             System.out.print("Enter Bonus: ");
-             Double bonus = Double.valueOf(sc.nextLine());
+        s.displayStudent();
 
-             if (salary < 0 || bonus < 0) {
-                 System.out.println("Invalid salary or bonus");
-             } else {
-                 Double netSalary = salary + bonus;
+        System.out.println();
 
-            System.out.println("Employee ID: " + id);
-            System.out.println("Basic Salary: Rs." + salary);
-            System.out.println("Bonus: Rs." + bonus);
-            System.out.println("Net Salary: Rs." + netSalary);
-        }
-
-        sc.close();
+        f.displayFaculty();
     }
 }
-
-
